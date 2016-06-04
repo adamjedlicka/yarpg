560b5223e7b1dbbc65974d3025d3a5122a178d79
Adam Jedlička <adajedlicka@gmail.com>
2016-06-04 12:42:26 +0200

    Added Buffer to Engine

--- src/engine.cpp
+++ src/engine.cpp
@@ -11,10 +11,15 @@ Engine::Engine() {
 	curs_set(false);
 
 	this->log.open("log.txt");
+	for (uint16_t i = 0; i < MAX_KEYS; ++i)
+		this->keys[i] = false;
+
+	this->maxHeight = getmaxy(this->window);
+	this->maxWidth = getmaxx(this->window);
+	for (uint16_t i = 0; i < MAX_BUFFERS; ++i)
+		this->buffers[i] = new Buffer(this->maxWidth, this->maxHeight);
 
 	this->active = false;
-	this->tmpX = 0;
-	this->tmpY = 0;
 }
 
 Engine::~Engine() {
@@ -29,20 +34,73 @@ void Engine::Start() {
 
 void Engine::Stop() { this->active = false; }
 
+Engine::Buffer *Engine::GetCurBuffer() { return this->buffers[0]; }
+
+Engine::Buffer::Buffer(){};
+Engine::Buffer::Buffer(uint16_t width, uint16_t height) {
+	this->width = width;
+	this->height = height;
+	this->canvas = new char *[height];
+	for (uint16_t i = 0; i < height; ++i)
+		this->canvas[i] = new char[width];
+}
+
+Engine::Buffer::~Buffer() {
+	for (uint16_t i = 0; i < this->height; ++i)
+		delete[] this->canvas[i];
+	delete[] this->canvas;
+}
+
+void Engine::Buffer::DrawChar(uint16_t height, uint16_t width, char c) {
+	switch (c) {
+	case '\0':
+		break;
+	default:
+		this->canvas[height][width] = c;
+	}
+}
+
+void Engine::Buffer::ClearCanvas() {
+	for (uint16_t i = 0; i < this->height; ++i) {
+		for (uint16_t j = 0; j < this->width; j++) {
+			this->canvas[i][j] = ' ';
+		}
+	}
+}
+
 void Engine::loop() {
 	clock_t drawTime = clock();
 
 	while (this->active) {
+		// Store all key presses in keyboard for future querries
+		int c = 0;
+		for (uint16_t i = 0; i < MAX_KEYS; ++i)
+			this->keys[i] = false;
+		while (c = getch(), c != ERR)
+			this->keys[c] = true;
+
+		// Game logic and rendering
+		this->tick();
 		this->render();
 
 		// Calculate sleep time for the rest of the frame, to save CPU load
 		clock_t now = clock();
 		double tm = double(now - drawTime) / CLOCKS_PER_SEC;
-		usleep((1.0 / FPS - tm) * 1000000);
+		usleep((1.0 / MAX_FPS - tm) * 1000000);
 	}
 }
 
+void Engine::tick() {}
+
 void Engine::render() {
-	mvprintw(this->tmpY++, this->tmpX++, "Hello, World");
+	Buffer *buf = this->GetCurBuffer();
+	buf->ClearCanvas();
+
+	
+
+	for (uint16_t i = 0; i < buf->GetHeight(); ++i) {
+		mvprintw(i, 0, buf->GetLine(i));
+	}
+
 	refresh();
 }
--- src/engine.h
+++ src/engine.h
@@ -8,15 +8,38 @@
 #include <string>
 #include <unistd.h>
 
-const uint8_t FPS = 30; // Maximum FPS. Used to calculate sleep time between frames
+#define MAX_FPS 30   // Maximum FPS. Used to calculate sleep time between frames
+#define MAX_KEYS 512 // Maximum number of keys ncurses can return
+#define MAX_BUFFERS 1
 
 class Engine {
   private:
 	WINDOW *window;
 	std::ofstream log;
+	bool keys[MAX_KEYS];
 
+	uint16_t maxWidth, maxHeight;
 	bool active;
-	int tmpX, tmpY;
+
+	struct Buffer {
+	  private:
+		char **canvas;
+		uint16_t width, height;
+
+	  public:
+		Buffer();
+		Buffer(uint16_t, uint16_t);
+		~Buffer();
+
+		void DrawChar(uint16_t, uint16_t, char);
+		void ClearCanvas();
+
+		uint16_t GetWidth() const { return this->width; };
+		uint16_t GetHeight() const { return this->height; };
+		char GetChar(uint16_t h, uint16_t w) const { return this->canvas[h][w]; };
+		const char *GetLine(uint16_t h) const { return this->canvas[h]; };
+	};
+	Buffer *buffers[MAX_BUFFERS];
 
   public:
 	Engine();
@@ -25,8 +48,11 @@ class Engine {
 	void Start();
 	void Stop();
 
+	Buffer *GetCurBuffer();
+
   private:
 	void loop();
+	void tick();
 	void render();
 };
 
